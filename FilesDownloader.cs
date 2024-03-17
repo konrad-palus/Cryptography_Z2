48f647db59a3fd032062ddf93f04118ce39ddf82
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-03-17 23:44:19 +0100

    Ściągnąłem  plik i sprawdziłem, czy hash się zgadza.

---
+++ FilesDownloader.cs
@@ -0,0 +1,17 @@
+﻿namespace Z2
+{
+    public class FilesDownloader
+    {
+        public static async Task DownloadFileAsync(string fileUri, string outputPath)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync(fileUri, HttpCompletionOption.ResponseHeadersRead);
+
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            using var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true);
+            await stream.CopyToAsync(fileStream);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,12 +2,31 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Provide text");
+            Console.WriteLine("Provide text:");
             Hashing hashingExample = new Hashing();
 
-            hashingExample.HashInput(Console.ReadLine()!);
+            string inputText = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(inputText))
+            {
+                hashingExample.HashInput(inputText);
+            }
+
+            string userFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string directoryPath = Path.Combine(userFolderPath, "x");
+            string fileName = "ubuntu.iso"; 
+            string outputPath = Path.Combine(directoryPath, fileName);
+
+            Directory.CreateDirectory(directoryPath);
+
+            Console.WriteLine("Downloading the Ubuntu image..");
+            string fileUri = "https://releases.ubuntu.com/23.10.1/ubuntu-23.10.1-desktop-amd64.iso.torrent?_gl=1*72v173*_gcl_au*MTkyMzM0MTQ5OC4xNzEwNzE0MjQ3&_ga=2.210266005.1285547986.1710714240-1230512047.1710714240";
+            await FilesDownloader.DownloadFileAsync(fileUri, outputPath);
+            Console.WriteLine("Download completed.");
+
+            Console.WriteLine("Hashing the downloaded file...");
+            hashingExample.HashInput(outputPath); 
         }
     }
 }
